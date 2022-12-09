60d37763743f32458b7d7f0b2197ce2370712f3b
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-09 21:30:44 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+/**
+* main - Entry 
+* return - 0
+*/
+int main(void)
+{
+  int fx;
+  int cg;
+  
+  for (fx = 0; fx < 10; fx++)
+    putchar((fx % 10) + '0');
+  
+  for (cg = 'a'; cg <= 'f'; cg++)
+    putchar(cg);
+  
+  putchar('\n');
+  
+  return(0);
+}
+  
