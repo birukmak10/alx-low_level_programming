c9cabc18da5ec4d6e5fce3c5d29d0e19cfe64654
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-09 20:44:26 +0300

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main: program that prints the alphabet in lowercase, and then in uppercase
+* return: 0
+*/
+int main(void)
+{
+  char dx;
+  
+  for (dx = 'a'; dx <= 'z'; dx++)
+    putchar(dx);
+  
+  for (dx = 'A'; dx <= 'Z'; dx++)
+    putchar(dx);
+  
+  putchar('\n');
+  
+  return(0);
+}
+    
