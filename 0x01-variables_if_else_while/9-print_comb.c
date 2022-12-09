01c08a30a32b04220f1c57a6b137cc6193ae52ec
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-09 21:34:53 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+/**
+* main - program that prints all possible combinations of single-digit numbers. 
+* return - 0
+*/
+int main(void)
+{
+  int fx;
+  for (fx = 48; fx < 58; fx++)
+  {
+    putchar(fx);
+    if (fx != 57)
+    {
+         putchar(',');
+         putchar(' ');
+    }
+    putchar('\n');
+    return (0);
+    
+}
