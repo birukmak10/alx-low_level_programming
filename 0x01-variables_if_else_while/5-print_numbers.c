3bda72c476697e964ff3b0976bef004d61438ed9
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-09 21:18:52 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+/**
+* main - program that prints all single digit numbers of base 10 starting from 0
+* return - 0
+*/
+int main(void)
+{
+  int gr;
+  for (gr = 0; gr < 10; gr++)
+    print("%d",gr);
+  print("\n");
+  return(0);
+}
