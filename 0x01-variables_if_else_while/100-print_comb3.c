1d17c0707f67a767b73040d350bde962b77e76b9
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-12 11:13:17 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 
 /**
-* main: program that prints the alphabet in lowercase 
+* main - program that prints all possible different combinations of two digits
 *
 * return: 0
+*
 */
 int main(void)
 {
+     int a = '0';
+     int b = '0';
+  
+     for (b='0' ; b <= '9' ; b++)
+     {
+        for ( a = '0'; a <= '9'; a++)
+        {
+           if (!(( a == b) ||  (b > a)))
+           {
+             putchar(b);
+             putchar(a);
+             if (!(a == '9' && b == '8'))
+             {
+                 putchar(',');
+                 putchar(' ');
+             }
+           }
+        }
+     }
+    putchar('\n');
+    return (0);
+}
