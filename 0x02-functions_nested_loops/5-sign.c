2a1a2e2a1743c33a6ac230f2a148bdc270842ddf
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-14 12:17:06 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,12 +14,12 @@ int print_sign(int c)
   }
   else if( c == 0)
   {
-    _putchar('0')
+    _putchar('0');
     return (0);
   }  
   else
   {
     _putchar('-');
-    return (1);
+    return (-1);
   }
 }
