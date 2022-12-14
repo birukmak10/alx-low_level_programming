62f6b2ae7f8257949167b9f5252d86ecb3b22322
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-14 12:58:29 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,12 @@
  */
 int print_last_digit(int n)
 {
-  int l = n % 10;
+  int l;
+  l = n% 10;
   if(l<0)
   {
-    l *= -1;
+    l = l * -1;
   }
   _putchar(l + '0');
-  return (0);
+  return (l);
 }
