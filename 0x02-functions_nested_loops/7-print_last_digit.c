b71f870b36dc4ede9eb560eff3169ca42b085aea
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-14 12:22:13 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,17 @@
- 
+#include "main.h"
+
+/**
+ * main -  a function that prints the last digit of a number
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int c)
+{
+  int x = c % 10;
+  
+  if (x<0)
+    x *= -1;
+  _putchar(x + '0');
+
+  return (0);
+}
