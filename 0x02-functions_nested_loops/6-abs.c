6770efb85425febbe4b6ca7c11396de619afead1
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-14 12:19:00 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,18 @@
- 
+#include "main.h"
+
+/**
+ * main -  a function that computes the absolute value of an intege
+ *
+ * Return: Always 0.
+ */
+int _abs(int c)
+{
+  if(c >= 0)
+  {
+    return (c);
+  }  
+  else
+  {
+    return (-c);
+  }
+}
