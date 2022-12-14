f419f76c564882a8708134aebf87a3382bafa00b
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-14 11:37:24 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,14 @@
- 
+#include main.h
+#include <unistd.h>
+
+/**
+ *  program that prints _putchar
+ * @c: The character to print
+ * 
+ *  Return: on success 1
+ * /
+
+ int _putchar(char c)
+ {
+      return (write(1, &c, 1));
+ }
