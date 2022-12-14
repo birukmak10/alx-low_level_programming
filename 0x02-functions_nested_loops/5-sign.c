fdae89a62c8fe9759c5836430587c2d11a78ac6c
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-14 12:15:45 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,25 @@
- 
+#include "main.h"
+
+/**
+ * main - a function that prints the sign of a number
+ *
+ * Return: Always 0.
+ */
+int print_sign(int c)
+{
+  if(c > 0)
+  {
+    _putchar('+');
+    return (1);
+  }
+  else if( c == 0)
+  {
+    _putchar('0')
+    return (0);
+  }  
+  else
+  {
+    _putchar('-');
+    return (-1);
+  }
+}
