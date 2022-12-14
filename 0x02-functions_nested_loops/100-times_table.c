c822d377ac9c14914727d39e5af8bed97ffd11ad
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-14 13:26:10 +0300

    Create 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main -  The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  print_times_table(3);
+  _putchar('\n');
+  print_times_table(5);
+  _putchar('\n');
+  print_times_table(98);
+  _putchar('\n');
+  print_times_table(12);
+  return (0);
+}
