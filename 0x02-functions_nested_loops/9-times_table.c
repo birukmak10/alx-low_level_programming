a9dee7da04539e85aad7c3bd3ac2aa533ee40605
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-14 12:40:00 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,34 @@
- 
+#include "main.h"
+
+/**
+ * main -  a function that prints the 9 times table, starting with 0
+ *
+ * Return: Always 0.
+ */
+void time_table(void)
+{
+  int num, mul, prd;
+  for (num = 0; num <= 9;num++)
+  {
+    _putchar('0');
+
+    for (mul = 1; mul <= 9; mul++)
+    {
+      _putchar(',');
+      _putchar(' ');
+
+      prd = num + mul;
+
+      if (prd<=9)
+      {
+        _putchar(' ');
+      }
+      else
+      {
+        _putchar((prd / 10) + '0');
+      }
+      _putchar((prd % 10) + '0');
+    }
+    _putchar('\n');
+  }
+}
