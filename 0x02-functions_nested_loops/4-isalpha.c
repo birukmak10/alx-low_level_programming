5879fa75efb14ba6c50747b9dc74397b0c57d96e
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-14 12:12:08 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,18 @@
- 
+#include "main.h"
+
+/**
+ * main - a function that checks for alphabetic characte
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int r)
+{
+  if((r>='a' && r <='z')|| (r>='A' && r <='Z'))
+  {
+    return (1);
+  }
+  else
+  {
+    return (0);
+  }  
+} 
