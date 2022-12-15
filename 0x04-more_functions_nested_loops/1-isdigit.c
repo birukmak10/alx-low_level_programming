3add88c4de641ad7d1bb56f7a1f99b1b94e9e69f
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-15 12:06:55 +0300

    Task_2

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,21 @@
- 
+ #include "main.h"
+
+/**
+ * _isdigit - check if numbers are 0 -9
+ * @x: char to check
+ * Return: 0 or 1
+*/
+
+int _isdigit(int x)
+{
+if (x >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
+
