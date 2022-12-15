f088b4e7b55dd998691cf17880cee38fe11ed6a1
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-15 11:59:30 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,12 +8,12 @@
 
 int _isupper(int x)
 {
-    if (x>=65 && x <= 90)
-		    {
-           return (1);
-    }
-    else
-    {
-          return (0);
-    }
+if (x>=65 && x <= 90)
+{
+   return (1);
+}
+else
+{
+   return (0);
+}
 }
