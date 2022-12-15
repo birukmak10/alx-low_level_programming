b431d5e7e9f6e48fb65976e01d5259cfa3aaf0e7
birukmak10 <birukmekonen10@gmail.com>
2022-12-15 00:57:54 -0800

    Task_1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,19 @@
- 
+#include "main.h"
+
+/**
+* _isupper - checks if parameter is an uppercase character.
+* @x: input character.
+* Return: 1 if an uppercase character , 0 if lowercase.
+* */
+
+int _isupper(int x)
+{
+    if (x>=65 && x <= 90)
+		    {
+           return (1);
+    }
+    else
+    {
+          return (0);
+    }
+}
