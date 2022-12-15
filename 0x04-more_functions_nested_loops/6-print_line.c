e21f89f8ad81da108d7a8653ab2b7c2f1d577a6f
birukmak10 <birukmekonen10@gmail.com>
2022-12-15 13:24:27 +0300

    print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,16 @@
- 
+  #include "main.h"
+
+/**
+ * print_line - function that draws a straight line in the termina
+ * @x: straight line is printed
+ * Return: no return
+*/
+void print_line(int x)
+{
+int i;
+for (i = 0; i < x; i++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
