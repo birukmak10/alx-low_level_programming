07c744d5933c323699dcc550c6d02833323097f3
birukmak10 <birukmekonen10@gmail.com>
2022-12-15 13:10:51 +0300

    More_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,21 @@
- 
+ #include "main.h"
+
+/**
+ * more_numbers - print more numbers or integer
+*/
+
+void more_numbers(void)
+{
+int i,j;
+for (i = 1; i <= 10; i++)
+{
+    for ( j = 0; i <=14; j++)
+    {
+       if (j>=10)
+       {
+           _putchar('1');
+       }
+       _putchar('\n');
+        }
+}
+}
