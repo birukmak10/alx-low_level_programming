324361b4bbf775a52a4812df29cce96ef64956f5
birukmak10 <birukmekonen10@gmail.com>
2022-12-15 00:50:12 -0800

    main

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef main_h
+#define main_
+
+int _putchar(char n);
+int _isupper(int n);
+int _isdigit(int n);
+int mul(int a ,int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int s);
+void print_triangle(int s);
+void print_number(int n);
+
+#endif
