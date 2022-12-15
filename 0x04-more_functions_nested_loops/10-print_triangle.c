3d24ca432c93327c4b97c7fd251f10246e7cc26a
birukmak10 <birukmekonen10@gmail.com>
2022-12-15 13:56:05 +0300

    Task_10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,22 +7,21 @@
  */
 void print_triangle(int x)
 {
-    int i, j;
-    for (i = 0; i < x; i++)
-    {
-        for (j = 1; j < (x - 1); j++)
-        {
-            _putchar(' ');
-        }
-        for (j-- ; j < x; j++)
-        {
-            _putchar(35);
-        }
-        if(i < (x - 1))
-        {
-            _putchar('\n');
-        }
-        
-    }
-    _putchar('\n');
+int i, j;
+for (i = 0; i < x; i++)
+{
+for (j = 1; j < (x - 1); j++)
+ {
+_putchar(' ');
+}
+for (j-- ; j < x; j++)
+{
+_putchar(35);
+}
+if(i < (x - 1))
+{
+_putchar('\n');
+}       
+}
+_putchar('\n');
 }
