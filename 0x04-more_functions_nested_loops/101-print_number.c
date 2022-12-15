0fde4ece57c19643143e4e76cb608c1f01453f2b
birukmak10 <birukmekonen10@gmail.com>
2022-12-15 16:29:06 +0300

    Advanced_2

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @x: input integer
+ * Return: no return
+ */
+void print_number(int x)
+{
+    unsigned int m, d, count;
+    if (x <0)
+    {
+        _putchar(45);
+        m = x * -1;
+    }
+    else
+    {
+        m = x;
+    }
+    d = m;
+    count = 1;
+    while (d > 9)
+    {
+        d /= 10;
+        count *= 10;
+    }
+    for (count; count >= 1;count /= 10)
+    {
+        _putchar(((m / count) % 10) + 48);
+    }
+}
