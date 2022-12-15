9f0ce65d5398fba072087cbf30053e819c3eb95e
birukmak10 <birukmekonen10@gmail.com>
2022-12-15 16:18:53 +0300

    Advanced_1

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints largest prime factor
+ * Return: Always 0
+ */
+
+int main(void)
+{
+    long int x, y;
+    x = 612852475143;
+    for (y = 2; y <= x; y++)
+    {
+        if (x % y == 0)
+        {
+            x /= y;
+            y++;
+        }
+    }
+    printf("%ld\n", y);
+    return (0);
+}
