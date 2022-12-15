3b86c79e480ac5b1b244b87008a86d8cccfdd2ec
birukmak10 <birukmekonen10@gmail.com>
2022-12-15 13:14:16 +0300

    More_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,16 @@
 
 void more_numbers(void)
 {
-int i,j;
-for (i = 1; i <= 10; i++)
+int i, j;
+for(i = 1; i <= 10; i++)
 {
-    for ( j = 0; i <=14; j++)
-    {
-       if (j>=10)
-       {
-           _putchar('1');
-       }
-       _putchar('\n');
-        }
+for ( j = 0; i <=14; j++)
+{
+if (j>=10)
+{
+_putchar('1');
+}
+_putchar('\n');
+}
 }
 }
