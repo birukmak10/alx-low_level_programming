df6c15c65dd53520962ce642433982da737496ce
birukmak10 <birukmekonen10@gmail.com>
2022-12-15 13:46:58 +0300

    Task_9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,33 @@
+ #include <stdio.h>
+ /**
+ * main -function that prints a square, followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+    int i;
+    i = 1;
+    printf("%d", i);
+    for (i = 2; i <= 100; i++)
+    {
+        if ((i % 3 == 0) && (i % 5 == 0))
+        {
+            printf(" FizzBuzz");
+        }
+        else if (i % 3 == 0)
+        {
+            printf(" Fizz");
+        }
+        else if (i % 5 ==0)
+        {
+            printf(" Buzz");
+        }
+        else
+        {
+            printf(" %d", i);
+        }
+    }
+    printf('\n');
+    return (0);
+}
  
