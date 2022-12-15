9aee553e3a86a29b77cc0555e81c54d83339a66e
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-15 12:24:22 +0300

    Task_4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,20 @@
- 
+#include "main.h"
+
+/**
+ * print_most_numbers -print numbers between 0-9
+ * Return: no return
+*/
+
+int print_numbers(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+if (i != 50 && i !=52)
+{
+_putchar(i);
+}
+}
+_putchar('\n');
+}
+
