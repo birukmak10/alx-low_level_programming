6b1961db9278fbb58dd261e14efeadefefe8acbd
birukmak10 <birukmekonen10@gmail.com>
2022-12-16 13:35:38 +0300

    Task_4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,12 @@
 void print_numbers(void)
 {
 int i;
-for (i = 48; i < 58; i++)
+for (i = 0; i <= 9; i++)
 {
-if (i != 50 && i !=52)
-{
-_putchar(i);
-}
+if ((i == 2) || (i == 4))
+continue;
+else
+_putchar(i + '0');
 }
 _putchar('\n');
 }
