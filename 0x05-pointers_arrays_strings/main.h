a13246b7d6e0492f226728c07c21d65d238e43a0
birukmak10 <birukmekonen10@gmail.com>
2022-12-19 20:55:54 +0300

    Update

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,16 +4,16 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void reset_to_98(int *x);
+void reset_to_98(int *c);
 void swap_int(int *a, int *b);
 int _strlen(char *str);
 void _puts(char *str);
-int _putchar(char *s);
-void print_rev(char *str);
+int _putchar(char *str);
+void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int x);
 char *_strcpy(char *dest, char *src);
-void rev_string(char *x);
+void rev_string(char *s);
 
 #endif
