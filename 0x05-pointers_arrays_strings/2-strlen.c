d2ab8a043a3a48403699ffe76a27c5336ba3c074
birukmak10 <birukmekonen10@gmail.com>
2022-12-19 20:01:51 +0300

    Task_2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @a: input
+ * Return; Length of the string
+ */
+
+void _strlen(char *a)
+{
+    int i;
+    for (i = 0; a[i] != '\0'; i++)
+    {
+        return (i);
+    }
+}
