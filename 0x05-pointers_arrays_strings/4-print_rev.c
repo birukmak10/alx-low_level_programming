06608641f2f1442285b40fb2ea13ebc1a2b91ee4
birukmak10 <birukmekonen10@gmail.com>
2022-12-19 20:30:33 +0300

    Task_4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse, followed by a new line
+ * @x: input string
+ */
+void print_rev(char *x)
+{
+    int i;
+    for (i = 0; x[i] != '\0'; i++)
+        ;
+    for (i = i -1; x[i] != '\0'; i--)
+    {
+        _putchar(x[i]);
+    }
+    _putchar('\n');
+}
