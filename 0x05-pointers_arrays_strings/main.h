95dbc61622b889124fd16cb27862c998c1e1d590
birukmak10 <birukmekonen10@gmail.com>
2022-12-19 20:50:37 +0300

    main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
 void reset_to_98(int *x);
 void swap_int(int *a, int *b);
 int _strlen(char *str);
@@ -9,3 +15,5 @@ void puts_half(char *str);
 void print_array(int *a, int x);
 char *_strcpy(char *dest, char *src);
 void rev_string(char *x);
+
+#endif
