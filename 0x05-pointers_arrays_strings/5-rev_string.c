dc14f21b9f1f6f35dd35b84f01fa86defe0c5079
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-20 10:56:09 +0300

    Task_5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - function that reverses a string
+ * @x: input string
+ * Return: void
+ */
+void rev_string(char *x)
+{
+int len = 0, i = 0;
+char j;
+while (x[i++])
+len++;
+for (i = len - 1; i >= len / 2; i--)
+{
+j = x[i];
+x[i] = x[len - i -1];
+x[len - i - 1] = j;
+}
+}
