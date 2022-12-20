3a5805689ebbd8b521fc0bec0eab02e6788d3144
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-20 12:55:41 +0300

    Task_10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,28 +8,28 @@
 
 int _atoi(char *x)
 {
-    int i = 0;
-    int j = 0;
-    int min = 1;
-    unsigned int n = 0;
-    while (x[i])
-    {
-        if(x[i] == 45)
-        {
-            min *= -1;
-        }
-        while (x[i] >= 48 && x[i] <= 57)
-        {
-            j = 1;
-            n = (n * 10) + (x[i] - '0');
-            i++;
-        }
-        if (j == 1)
-        {
-            break;
-        }
-        i++;
-    }
-    n *= min;
-    return (n);
+int i = 0;
+int j = 0;
+int min = 1;
+unsigned int n = 0;
+while (x[i])
+{
+if(x[i] == 45)
+{
+min *= -1;
+}
+while (x[i] >= 48 && x[i] <= 57)
+{
+j = 1;
+n = (n * 10) + (x[i] - '0');
+i++;
+}
+if (j == 1)
+{
+break;
+}
+i++;
+}
+n *= min;
+return (n);
 }
