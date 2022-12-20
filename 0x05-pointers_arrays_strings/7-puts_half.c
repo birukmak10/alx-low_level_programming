cc0412ec73cbb17d4d2376c38dde4a741db3b7a4
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-20 11:21:12 +0300

    Task_7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 -  function that prints half of a string
+ * @str: input string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int i, j;
+j = 0;
+while (str[j] != '\0')
+{
+j++;
+}
+if (j % 2 ==1)
+{
+i = (j - 1) / 2;
+i += q;
+}
+_putchar('\n');
+}
