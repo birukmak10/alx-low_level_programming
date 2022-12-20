b541257c75fa352cc9ba065ef2ce702a2b3e9cf6
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-20 11:17:46 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_rev -prints a string,in reverse,followed by a new line
+ * print_rev - prints a string,in reverse,followed by a new line
  * @x: input string
  */
 void print_rev(char *x)
