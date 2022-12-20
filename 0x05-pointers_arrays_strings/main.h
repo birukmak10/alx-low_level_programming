92928dd99ffaa72c73d7c0573e45dee941d2c092
birukmak10 <66904949+birukmak10@users.noreply.github.com>
2022-12-20 11:12:59 +0300

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,14 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <unistd.h>
-
 void reset_to_98(int *c);
 void swap_int(int *a, int *b);
 int _strlen(char *str);
 void _puts(char *str);
-int _putchar(char *str);
+int _putchar(char str);
 void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str);
