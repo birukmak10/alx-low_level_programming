5da176007585ccab7a77f2c53ba6c386eebd1b80
birukmak10 <birukmekonen10@gmail.com>
2022-12-22 10:22:17 +0300

    Task_0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,9 +11,15 @@
 char *_strcat(char *dest, const char *src)
 {
 int index = 0, dest_len = 0;
-while (dest[index++])
+while (dest[index])
+{
 dest_len++;
+}
 for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
+{
+dest[dest_len] = src[index];
+dest_len++;
+}
+dest[dest_len]= '\0';
 return (dest);
 }
