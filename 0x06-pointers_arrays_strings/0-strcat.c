f524ff01d06f46b5264377e4b3f01224499f5429
birukmak10 <birukmekonen10@gmail.com>
2022-12-22 10:30:11 +0300

    Task_0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,18 +9,12 @@
  *
  * Return: A pointer to the destination string @dest
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, const char *src)
 {
 int index = 0, dest_len = 0;
-while (dest[index])
-{
+while (dest[index++])
 dest_len++;
-}
 for (index = 0; src[index]; index++)
-{
-dest[dest_len] = src[index];
-dest_len++;
-}
-dest[dest_len]= '\0';
+dest[dest_len++] = src[index];
 return (dest);
 }
