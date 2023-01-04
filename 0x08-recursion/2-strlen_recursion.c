cdbdb27c2827b83eccbd821110e67a749a0fb3f8
birukmak10 <birukmekonen10@gmail.com>
2023-01-04 08:45:13 +0300

    Task_2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,13 +8,11 @@
  */
 int _strlen_recursion(char *s)
 {
-        int len = 0;
-
-        if (*s)
-        {
-                len++;
-                len += _strlen_recursion(s + 1);
-        }
-
-        return (len);
+int len = 0;
+if (*s)
+{
+len++;
+len += _strlen_recursion(s + 1);
+}
+return (len);
 }
