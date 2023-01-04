59e153d5d56f2ce8bbb090a0c5dab8ca46f2af9f
birukmak10 <birukmekonen10@gmail.com>
2023-01-04 08:46:50 +0300

    Task_4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,15 +9,11 @@
  */
 int _pow_recursion(int x, int y)
 {
-        int result = x;
-
-        if (y < 0)
-                return (-1);
-
-        else if (y == 0)
-                return (1);
-
-        result *= _pow_recursion(x, y - 1);
-
-        return (result);
+int result = x;
+if (y < 0)
+return (-1);
+else if (y == 0)
+return (1);
+result *= _pow_recursion(x, y - 1);
+return (result);
 }
